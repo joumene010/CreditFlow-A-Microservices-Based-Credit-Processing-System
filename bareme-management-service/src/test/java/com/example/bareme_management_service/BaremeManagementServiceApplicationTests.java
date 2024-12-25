a1ebc543ea3014d80e65a6a341c2dbfb0d05a2b2
joumene010 <joumenbensaid@gmail.com>
2024-12-25 10:29:57 +0100

    Initial commit

---
+++ bareme-management-service/src/test/java/com/example/bareme_management_service/BaremeManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bareme_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaremeManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
